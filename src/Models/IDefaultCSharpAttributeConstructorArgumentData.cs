054e4448ea56f290037b5f4483e16df5b765fcd4
ErikWe <erikwestberg7@gmail.com>
2024-08-06 14:19:50 +0200

    Provide argument-data for different argument kinds

---
+++ src/Models/IDefaultCSharpAttributeConstructorArgumentData.cs
@@ -0,0 +1,8 @@
+﻿namespace Paraminter.Arguments.CSharp.Attributes.Constructor.Models;
+
+using Paraminter.Associators.Models;
+
+/// <summary>Represents syntactic data about a default C# attribute constructor argument.</summary>
+public interface IDefaultCSharpAttributeConstructorArgumentData
+    : IArgumentData
+{ }
---
+++ src/Models/INormalCSharpAttributeConstructorArgumentData.cs
@@ -0,0 +1,13 @@
+﻿namespace Paraminter.Arguments.CSharp.Attributes.Constructor.Models;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Paraminter.Associators.Models;
+
+/// <summary>Represents syntactic data about a normal C# attribute constructor argument.</summary>
+public interface INormalCSharpAttributeConstructorArgumentData
+    : IArgumentData
+{
+    /// <summary>The syntactic data about the normal C# attribute constructor argument.</summary>
+    public abstract AttributeArgumentSyntax SyntacticArgument { get; }
+}
